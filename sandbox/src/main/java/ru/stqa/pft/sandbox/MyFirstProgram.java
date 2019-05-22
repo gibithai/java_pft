5aa87258ce5f6a069762dd50cdf8109e92f8598f
gibithai <40383356+gibithai@users.noreply.github.com>
2019-05-22 23:59:11 +0300

    проект отвязан от репозитория GIT

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,9 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-public static void main (String[] args) {
-	
-System.out.println ("hello world");
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+    }
 }
-}
+
