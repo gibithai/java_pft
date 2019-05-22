28551b9d0ce81d7d4482133f24c3069b05e12dc9
gibithai <40383356+gibithai@users.noreply.github.com>
2019-05-23 00:41:53 +0300

    переменные

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,7 +4,10 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        System.out.println("hello world");
+        System.out.println("hello," + "world");
+        int l = 8;
+        int s = l *1;
+        System.out.println("площадь квадрата со стороной " + l + " = " +s);
     }
 }
 
