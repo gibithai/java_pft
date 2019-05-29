588b32a2fd6f6d2df31d402678e019bda98e9ad3
gibithai <40383356+gibithai@users.noreply.github.com>
2019-05-29 14:31:50 +0300

    Corrected mistake

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -13,16 +13,23 @@ public class GroupCreationTests {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/group.php");
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.id("LoginForm")).submit();
   }
 
   @Test
-  public void testGroupCreation() throws Exception  {
-    wd.get("http://localhost/addressbook/group.php");
+  public void testGroupCreation() throws Exception {
+
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).click();
-    /* ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=group_name | ]] */
+    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=group_name | ]]
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
     wd.findElement(By.name("group_name")).sendKeys("Test1");
